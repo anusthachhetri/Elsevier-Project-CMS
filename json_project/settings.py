cc6b66f3b8c6ab35e5ca7be772f04ab086ad2ea8
Anustha Chhetri <MAC012593@macmill.com>
2025-03-11 17:59:43 +0530

    updates settings

--- json_project/settings.py
+++ json_project/settings.py
@@ -209,10 +209,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-     #BASE_DIR / "static",
-     os.path.join(BASE_DIR,"json_app", "static"), #include the static folder
-]
+# STATICFILES_DIRS = [
+#      #BASE_DIR / "static",
+#      os.path.join(BASE_DIR,"json_app", "static"), #include the static folder
+# ]
 # Static root (for production)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
